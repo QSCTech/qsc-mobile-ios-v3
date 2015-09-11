0230f5ba944b286dcc0683fb35b9861d44445f5f
Axure <axurez@vip.qq.com>
2015-09-11 19:30:50 +0800

    Initial Commit

---
+++ QSCMobileV3/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  QSCMobileV3
+//
+//  Created by 郑虎 on 15 年 9. 11..
+//  Copyright © 2015年 QSC Tech. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ QSCMobileV3/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  QSCMobileV3
+//
+//  Created by 郑虎 on 15 年 9. 11..
+//  Copyright © 2015年 QSC Tech. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
