201348ee6c9337f49aa4f52bec0b598ffa624a97
Sun Yaozhu <yzyzsun@gmail.com>
2015-12-09 23:51:21 +0800

    Add SSID helper

--- QSCMobileKit/ZJUWLAN/ZjuwlanConnection.swift
+++ QSCMobileKit/ZJUWLAN/ZjuwlanConnection.swift
@@ -7,9 +7,24 @@
 //
 
 import Foundation
+import SystemConfiguration.CaptiveNetwork
 
 class ZjuwlanConnection: NSObject {
     
-    
+    /// Get SSID of current network, or an empty string. Note this method uses deprecated APIs since iOS 9.0.
+    static var currentSSID: String {
+        var ssid = ""
+        if let interfaces = CNCopySupportedInterfaces() {
+            let interfaces = interfaces as Array
+            for interfaceName in interfaces {
+                let interfaceName = String(interfaceName)
+                if let networkInfo = CNCopyCurrentNetworkInfo(interfaceName) {
+                    let networkInfo = networkInfo as Dictionary
+                    ssid = networkInfo[kCNNetworkInfoKeySSID] as! String
+                }
+            }
+        }
+        return ssid
+    }
     
 }
