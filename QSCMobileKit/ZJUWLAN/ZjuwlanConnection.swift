6b68d5d301dc1671b884ec061477643b39d87198
Sun Yaozhu <yzyzsun@gmail.com>
2015-12-09 21:45:57 +0800

    Init ZJUWLAN utils

---
+++ QSCMobileKit/ZJUWLAN/ZjuwlanConnection.swift
@@ -0,0 +1,15 @@
+//
+//  ZjuwlanConnection.swift
+//  QSCMobileV3
+//
+//  Created by 孙耀珠 on 2015-12-09.
+//  Copyright © 2015年 QSC Tech. All rights reserved.
+//
+
+import Foundation
+
+class ZjuwlanConnection: NSObject {
+    
+    
+    
+}
