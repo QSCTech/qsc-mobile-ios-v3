e342ddcf80293a84b902813aeba869d5e9891204
Sun Yaozhu <yzyzsun@gmail.com>
2016-05-09 16:02:00 +0800

    Test framework API

--- QSCMobileV3/TodayViewController.swift
+++ QSCMobileV3/TodayViewController.swift
@@ -11,13 +11,31 @@ import QSCMobileKit
 
 class TodayViewController: UIViewController {
 
+    let accountManager = AccountManager.sharedInstance
     let mobileManager = MobileManager.sharedInstance
+    let calendarManager = CalendarManager.sharedInstance
     
     @IBOutlet weak var textView: UITextView!
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-        mobileManager.refreshAll { _ in }
+        
+        if accountManager.currentAccountForJwbinfosys == nil {
+            mobileManager.loginValidate("freshman", "ios") { status, error in
+                if status {
+                    self.textView.text = "Login successfully"
+                } else {
+                    self.textView.text = error
+                }
+            }
+        } else {
+            mobileManager.refreshAll {
+                let date = NSDate(timeIntervalSinceNow: 0)
+                self.textView.text = self.mobileManager.coursesForDate(date).description
+                self.textView.text += "\n" + self.calendarManager.adjustmentForDate(date).debugDescription
+                self.textView.text += "\n" + self.calendarManager.holidayForDate(date).debugDescription
+            }
+        }
     }
     
     /*
