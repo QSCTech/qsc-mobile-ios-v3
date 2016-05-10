d1f59ea2179f23d8e91ae2f23b4e3d466364d5a3
Sun Yaozhu <yzyzsun@gmail.com>
2016-05-10 11:30:58 +0800

    Add font awesome

--- QSCMobileV3/TodayViewController.swift
+++ QSCMobileV3/TodayViewController.swift
@@ -37,15 +37,5 @@ class TodayViewController: UIViewController {
             }
         }
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
