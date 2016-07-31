65a35d5cb2e7de0e8a06c9e549f958ca5ef9cb5f
Sun Yaozhu <yzyzsun@gmail.com>
2016-07-31 15:51:27 +0800

    Add discover webview

--- QSCMobileV3/Discover/DiscoverViewController.swift
+++ QSCMobileV3/Discover/DiscoverViewController.swift
@@ -2,38 +2,26 @@
 //  DiscoverViewController.swift
 //  QSCMobileV3
 //
-//  Created by 孙耀珠 on 2015-12-05.
-//  Copyright © 2015年 QSC Tech. All rights reserved.
+//  Created by 孙耀珠 on 2016-07-31.
+//  Copyright © 2016年 QSC Tech. All rights reserved.
 //
 
 import UIKit
-import Kingfisher
-import QSCMobileKit
+import WebKit
+
+let DiscoverURL = "https://discover.zjuqsc.com"
 
 class DiscoverViewController: UIViewController {
-
-    @IBOutlet weak var textView: UITextView!
-    @IBOutlet weak var imageView: UIImageView!
     
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-        let noticeManager = NoticeManager.sharedInstance
-        noticeManager.allEventsWithPage(1) { events, error in
-            guard let events = events else {
-                self.textView.text = error
-                return
-            }
-            self.textView.text = events.description
-            self.imageView.kf_setImageWithURL(events[0].sponsorLogoURL)
-            noticeManager.updateDetailWithEvent(events[0]) { event, error in
-                guard let event = event else {
-                    self.textView.text = error
-                    return
-                }
-                self.textView.text = event.content
-            }
-        }
+        let webView = WKWebView(frame: CGRect(x: 0, y: 20, width: view.frame.width, height: view.frame.height - 69))
+        webView.allowsBackForwardNavigationGestures = true
+        webView.scrollView.showsVerticalScrollIndicator = false
+        webView.scrollView.showsHorizontalScrollIndicator = false
+        webView.loadRequest(NSURLRequest(URL: NSURL(string: DiscoverURL)!))
+        view.addSubview(webView)
     }
-
+    
 }
