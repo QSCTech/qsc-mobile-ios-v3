134c93fc78f984bfabc6fc6736756d8dc733ca55
Sun Yaozhu <yzyzsun@gmail.com>
2016-09-01 19:06:31 +0800

    Enable keyboard enter to login

--- QSCMobileV3/Login/JwbinfosysLoginViewController.swift
+++ QSCMobileV3/Login/JwbinfosysLoginViewController.swift
@@ -27,12 +27,12 @@ class JwbinfosysLoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        passwordField.delegate = self
         navigationItem.title = "教务网账号"
         loginButton.layer.cornerRadius = 5
     }
     
     @IBAction func login(sender: AnyObject) {
-        view.endEditing(true)
         SVProgressHUD.showWithStatus("登录中")
         MobileManager.sharedInstance.loginValidate(usernameField.text!, passwordField.text!) { success, error in
             if success {
@@ -61,6 +61,15 @@ class JwbinfosysLoginViewController: UIViewController {
     
     @IBAction func close(sender: AnyObject) {
         dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+}
+
+extension JwbinfosysLoginViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        login(textField)
+        return false
     }
     
 }
