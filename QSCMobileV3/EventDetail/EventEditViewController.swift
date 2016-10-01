05d60bcf839c6ee03a2dd3c62460edc721f00e86
Sun Yaozhu <yzyzsun@gmail.com>
2016-10-02 01:06:58 +0800

    Update notification text

--- QSCMobileV3/EventDetail/EventEditViewController.swift
+++ QSCMobileV3/EventDetail/EventEditViewController.swift
@@ -329,8 +329,9 @@ class EventEditViewController: UITableViewController {
         
         if customEvent!.notification >= 0 {
             let notif = UILocalNotification()
-            let time = customEvent!.notification == 0 ? "现在" : "将于 " + customEvent!.notification!.stringFromNotificationType.stringByReplacingOccurrencesOfString("前", withString: "后")
-            notif.alertBody = "「\(customEvent!.name!)」\(time)开始"
+            let time = customEvent!.notification == 0 ? "马上" : "将于 " + customEvent!.notification!.stringFromNotificationType.stringByReplacingOccurrencesOfString("前", withString: "后")
+            let place = customEvent!.place == "" ? "" : "在 " + customEvent!.place! + " "
+            notif.alertBody = "「\(customEvent!.name!)」\(time)\(place)开始"
             notif.fireDate = customEvent!.start!.dateByAddingTimeInterval(-customEvent!.notification!.doubleValue)
             notif.soundName = UILocalNotificationDefaultSoundName
             notif.userInfo = ["objectID": customEvent!.objectID.URIRepresentation().URLString]
