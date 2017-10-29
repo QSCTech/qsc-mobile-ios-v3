05329c232415e305ddf228c5e5fd4ba7a85c89bd
Sun Yaozhu <yzyzsun@gmail.com>
2017-10-29 18:19:18 +0900

    Fix notice webview for iPhone X

--- QSCMobileV3/Discover/DiscoverViewController.swift
+++ QSCMobileV3/Discover/DiscoverViewController.swift
@@ -9,19 +9,26 @@
 import UIKit
 import WebKit
 
-let DiscoverURL = "https://notice.zjuqsc.com/ios/"
+let DiscoverURL   = URL(string: "https://notice.zjuqsc.com/mobile/")!
+let DiscoverColor = UIColor(red: 0.945, green: 0.643, blue: 0.122, alpha: 1.0)
 
 class DiscoverViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let webView = WKWebView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height - 49))
+        let webView = WKWebView(frame: CGRect.zero)
+        webView.translatesAutoresizingMaskIntoConstraints = false
         webView.allowsBackForwardNavigationGestures = true
         webView.scrollView.showsVerticalScrollIndicator = false
         webView.scrollView.showsHorizontalScrollIndicator = false
-        webView.load(URLRequest(url: URL(string: DiscoverURL)!))
+        webView.load(URLRequest(url: DiscoverURL))
         view.addSubview(webView)
+        view.addConstraint(webView.leadingAnchor.constraint(equalTo: view.leadingAnchor))
+        view.addConstraint(webView.trailingAnchor.constraint(equalTo: view.trailingAnchor))
+        view.addConstraint(webView.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor))
+        view.addConstraint(webView.bottomAnchor.constraint(equalTo: bottomLayoutGuide.topAnchor))
+        view.backgroundColor = DiscoverColor
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
