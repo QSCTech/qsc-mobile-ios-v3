18d7a61613beab67a093a6d312a66d669089f9f8
Yaozhu Sun <yzyzsun@gmail.com>
2019-09-08 15:28:31 +0800

    Update to Swift 5.0 and fix warnings

--- QSCMobileKit/Box/Date+Box.swift
+++ QSCMobileKit/Box/Date+Box.swift
@@ -10,32 +10,32 @@ import Foundation
 
 public extension Date {
     
-    public static let oneHourInSeconds: TimeInterval = 1 * 60 * 60
-    public static let oneDayInSeconds: TimeInterval = 24 * Date.oneHourInSeconds
-    public static let fiveDaysInSeconds: TimeInterval = 5 * Date.oneDayInSeconds
-    public static let tenDaysInSeconds: TimeInterval = 10 * Date.oneDayInSeconds
-    public static let thirtyDaysInSeconds: TimeInterval = 30 * Date.oneDayInSeconds
-    public static let expirationsInSeconds: [TimeInterval] = [oneHourInSeconds, oneDayInSeconds, fiveDaysInSeconds, tenDaysInSeconds, thirtyDaysInSeconds]
+    static let oneHourInSeconds: TimeInterval = 1 * 60 * 60
+    static let oneDayInSeconds: TimeInterval = 24 * Date.oneHourInSeconds
+    static let fiveDaysInSeconds: TimeInterval = 5 * Date.oneDayInSeconds
+    static let tenDaysInSeconds: TimeInterval = 10 * Date.oneDayInSeconds
+    static let thirtyDaysInSeconds: TimeInterval = 30 * Date.oneDayInSeconds
+    static let expirationsInSeconds: [TimeInterval] = [oneHourInSeconds, oneDayInSeconds, fiveDaysInSeconds, tenDaysInSeconds, thirtyDaysInSeconds]
     
-    public static func dateToShortString(date: Date) -> String {
+    static func dateToShortString(date: Date) -> String {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy.MM.dd"
         return dateFormatter.string(from: date)
     }
     
-    public static func dateToShortStringWithoutDots(date: Date) -> String {
+    static func dateToShortStringWithoutDots(date: Date) -> String {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyyMMdd"
         return dateFormatter.string(from: date)
     }
     
-    public static func dateToLongString(date: Date) -> String {
+    static func dateToLongString(date: Date) -> String {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy.MM.dd HH:mm:ss"
         return dateFormatter.string(from: date)
     }
     
-    public static func completeStringToDate(string: String) -> Date {
+    static func completeStringToDate(string: String) -> Date {
         let dateFormatter = DateFormatter()
         dateFormatter.locale = Locale(identifier: "en_US_POSIX")
         dateFormatter.dateFormat = "EEE, dd MMM yyyy HH:mm:ss zzz"
